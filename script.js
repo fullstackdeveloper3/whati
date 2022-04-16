8309d4cab15f7f1ffb36e0b8c82c81b2201794ae
unknown <abdullahhamada022@gmail.com>
2022-04-16 21:17:14 +0300

    tesr

---
+++ script.js
@@ -0,0 +1,32 @@
+let content = document.getElementById("content")
+let button = document.getElementById("show-ost")
+let mbutton = document.getElementById("mbutton")
+
+let barcontent = document.getElementById("barcontent")
+let sidebar = document.getElementById("sidebar")
+
+sidebar.onclick = function() {
+    if (barcontent.className === "right") {
+        barcontent.className = "left"
+        sidebar.innerHTML = `<i class="fa-solid fa-bars"></i>`
+    }else {
+        barcontent.className = "right"
+        sidebar.innerHTML = `<i class="fa-solid fa-xmark"></i>`
+    }
+}
+
+button.onclick = function () {
+    if (content.className === "one") {
+        content.className = "zero"
+    }else {
+        content.className = "one"
+    }
+}
+
+mbutton.onclick = function () {
+    if (content.className === "one") {
+        content.className = "zero"
+    }else {
+        content.className = "one"
+    }
+}
